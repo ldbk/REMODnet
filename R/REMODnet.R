9620118b1ba0fa646225ebaf7ae173df3aae3458
Laurent Dubroca <laurent.dubroca@gmail.com>
2017-11-15 19:43:54 +0100

    first step

---
+++ R/REMODnet.R
@@ -0,0 +1,12 @@
+#' REMODnet
+#'
+#' A package to gather and analyses data from EMODnet
+#'
+#'The European Marine Observation and Data Network (EMODnet) consists of more than 150 organisations assembling marine data, products and metadata to make these fragmented resources more available to public and private users relying on quality-assured, standardised and harmonised marine data which are interoperable and free of restrictions on use. EMODnet is currently in its second development phase with the target to be fully deployed by 2020.
+#'
+#' This package...
+#'
+#' @docType package
+#' @name REMODnet 
+NULL
+
