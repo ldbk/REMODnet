5a1284bca936bd62ad076fd2e67c062b448604e6
Laurent Dubroca <laurent.dubroca@gmail.com>
2017-11-17 11:26:21 +0100

    seabathy pb

--- R/getbathy.R
+++ R/getbathy.R
@@ -54,6 +54,7 @@ getbathy<-function(name="emodnet:mean",xmin=15,xmax=20.5,ymin=30,ymax=32.5){
 
   if(apply(checkparameter,1,sum)==2){
   	#define WCS connection"
+#name="emodnet:mean";xmin=-1;xmax1;ymin=49;ymax=51
         bbox<-paste(xmin,ymin,xmax,ymax,sep=",")
   	con <- paste0("http://ows.emodnet-bathymetry.eu/wcs?service=wcs&version=1.0.0&request=getcoverage&coverage=", 
 	              name,"&crs=EPSG:4326&BBOX=",bbox,"&format=image/tiff&interpolation=nearest&resx=0.00208333&resy=0.00208333") 
@@ -63,10 +64,22 @@ getbathy<-function(name="emodnet:mean",xmin=15,xmax=20.5,ymin=30,ymax=32.5){
   	utils::download.file(con,nomfich,quiet=TRUE,mode="wb")
   	#return the corresponding raster"
   	img<-raster::raster(nomfich)
-  	img[img==0]<-NA
-	img[img <0] <- 0
+  	#img[img==0]<-NA
+	#test<-raster::match(img,0)
+	#img<-raster::mask(img,test)
+	#img[img <0] <- 0
+	negval<-unique(raster::values(img))
+	negval<-negval[negval>=0]
+	test<-raster::match(img,negval)
+	img<-raster::mask(img,test)
+	negval<-unique(raster::values(img))
+	negval<-negval[negval<=stats::quantile(negval,.99,na.rm=T)]
+	test<-raster::match(img,negval)
+	img<-raster::mask(img,test)
+	#img<-raster::focal(img,w=matrix(1,ncol=11,nrow=11),fun=function(a){raster::mean(a)},NAonly=T)
+
 	#correction of strange value using quantile...
-	img[img >stats::quantile(img,.99)] <- 0
+	#img[img >stats::quantile(img,.99)] <- 0
   	#log inverse backtransform if chl or k490 data
   	#if(length(grep("log",data_gmis$unit[idvar],ignore.case=TRUE))>0){
    	#	img<-10^img
